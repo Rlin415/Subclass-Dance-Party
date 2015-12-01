58a26a61049930c0d0992b0342faa889aac4436f
Robert Lin <Rlin415@gmail.com>
2015-12-01 11:20:26 -0800

    add funky step dancer

---
+++ src/funkyStepDancer.js
@@ -0,0 +1,27 @@
+var FunkyStepDancer = function(top, left, timeBetweenSteps) {
+  FunkyDancer.call(this, top, left, timeBetweenSteps);
+  this.stepped = false;
+};
+
+FunkyStepDancer.prototype = {
+  constructor: FunkyStepDancer,
+  step: function(timeBetweenSteps) {
+    FunkyDancer.prototype.step.call(this, timeBetweenSteps);
+    var top = Number($(this.$node).css('top').slice(0, -2));
+    var left = Number($(this.$node).css('left').slice(0, -2));
+
+    if (this.stepped) {
+      $(this.$node).css({
+        top: top - 10,
+        left: left - 10
+      });
+      this.stepped = false;
+    } else {
+      $(this.$node).css({
+        top: top + 10,
+        left: left + 10
+      });
+      this.stepped = true;
+    }
+  }
+};
