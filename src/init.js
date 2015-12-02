5aa4e109f61e52433b268db4677d9ba4266fdaaa
Robert Lin <Rlin415@gmail.com>
2015-12-01 18:03:33 -0800

    made dancers interact

--- src/init.js
+++ src/init.js
@@ -1,5 +1,6 @@
 $(document).ready(function(){
   window.dancers = [];
+  window.taylor = {};
 
   $(".addDancerButton").on("click", function(event){
     /* This function sets up the click handlers for the create-dancer
@@ -16,17 +17,32 @@ $(document).ready(function(){
      * to the stage.
      */
     var dancerMakerFunctionName = $(this).data("dancer-maker-function-name");
-
     // get the maker function for the kind of dancer we're supposed to make
     var dancerMakerFunction = window[dancerMakerFunctionName];
 
     // make a dancer with a random position
+
+    if (window.taylor.top && dancerMakerFunctionName === 'TaylorDancer') {
+      alert('THERE CAN ONLY BE 1 TAYLOR!!');
+      return;
+    }
 
     var dancer = new dancerMakerFunction(
       $("body").height() * Math.random(),
       $("body").width() * Math.random(),
       Math.random() * 1000
     );
+
+    if (dancerMakerFunctionName === 'TaylorDancer') {
+      dancer.$node.css({top: 200, left: 400});
+      window.taylor.top = dancer.$node.css('top');
+      window.taylor.left = dancer.$node.css('left');
+    }
+
+    if (window.taylor.top) {
+      dancer.$node.css({'top': Number(window.taylor.top.slice(0, -2)) + Math.random() * 100});
+      dancer.$node.css({'left': Number(window.taylor.left.slice(0, -2)) + Math.random() * 100});
+    }
 
     window.dancers.push(dancer);
     $('body').append(dancer.$node);
